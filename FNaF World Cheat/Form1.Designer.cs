5c34f59726baf53b7fa7843e5bc1a90eb06a5962
kaykayeff <119964594+kaykayeff@users.noreply.github.com>
2023-02-22 16:09:33 +0200

    Add files via upload

---
+++ FNaF World Cheat/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace FNaF_World_Cheat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.getCharactersBTN = new System.Windows.Forms.Button();
+            this.slotBox = new System.Windows.Forms.TextBox();
+            this.slotlabel = new System.Windows.Forms.Label();
+            this.saveSlotBTN = new System.Windows.Forms.Button();
+            this.getChipsBTN = new System.Windows.Forms.Button();
+            this.getByte = new System.Windows.Forms.Button();
+            this.maxLevelBTN = new System.Windows.Forms.Button();
+            this.tokensBTN = new System.Windows.Forms.Button();
+            this.zonesBTN = new System.Windows.Forms.Button();
+            this.keyBTN = new System.Windows.Forms.Button();
+            this.armorBTN = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.getcharBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.charBox = new System.Windows.Forms.TextBox();
+            this.dataBTN = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // getCharactersBTN
+            // 
+            this.getCharactersBTN.Location = new System.Drawing.Point(3, 164);
+            this.getCharactersBTN.Name = "getCharactersBTN";
+            this.getCharactersBTN.Size = new System.Drawing.Size(226, 57);
+            this.getCharactersBTN.TabIndex = 0;
+            this.getCharactersBTN.Text = "Get all characters";
+            this.getCharactersBTN.UseVisualStyleBackColor = true;
+            this.getCharactersBTN.Click += new System.EventHandler(this.getCharactersBTN_Click);
+            // 
+            // slotBox
+            // 
+            this.slotBox.Location = new System.Drawing.Point(7, 39);
+            this.slotBox.Name = "slotBox";
+            this.slotBox.Size = new System.Drawing.Size(226, 22);
+            this.slotBox.TabIndex = 1;
+            // 
+            // slotlabel
+            // 
+            this.slotlabel.Location = new System.Drawing.Point(100, 9);
+            this.slotlabel.Name = "slotlabel";
+            this.slotlabel.Size = new System.Drawing.Size(81, 27);
+            this.slotlabel.TabIndex = 2;
+            this.slotlabel.Text = "Slot";
+            // 
+            // saveSlotBTN
+            // 
+            this.saveSlotBTN.Location = new System.Drawing.Point(7, 67);
+            this.saveSlotBTN.Name = "saveSlotBTN";
+            this.saveSlotBTN.Size = new System.Drawing.Size(226, 57);
+            this.saveSlotBTN.TabIndex = 3;
+            this.saveSlotBTN.Text = "Load slot";
+            this.saveSlotBTN.UseVisualStyleBackColor = true;
+            this.saveSlotBTN.Click += new System.EventHandler(this.saveSlotBTN_Click);
+            // 
+            // getChipsBTN
+            // 
+            this.getChipsBTN.Location = new System.Drawing.Point(235, 164);
+            this.getChipsBTN.Name = "getChipsBTN";
+            this.getChipsBTN.Size = new System.Drawing.Size(226, 57);
+            this.getChipsBTN.TabIndex = 4;
+            this.getChipsBTN.Text = "Get all chips";
+            this.getChipsBTN.UseVisualStyleBackColor = true;
+            this.getChipsBTN.Click += new System.EventHandler(this.getChipsBTN_Click);
+            // 
+            // getByte
+            // 
+            this.getByte.Location = new System.Drawing.Point(464, 164);
+            this.getByte.Name = "getByte";
+            this.getByte.Size = new System.Drawing.Size(226, 57);
+            this.getByte.TabIndex = 5;
+            this.getByte.Text = "Get all bytes";
+            this.getByte.UseVisualStyleBackColor = true;
+            this.getByte.Click += new System.EventHandler(this.getByte_Click);
+            // 
+            // maxLevelBTN
+            // 
+            this.maxLevelBTN.Location = new System.Drawing.Point(235, 227);
+            this.maxLevelBTN.Name = "maxLevelBTN";
+            this.maxLevelBTN.Size = new System.Drawing.Size(226, 57);
+            this.maxLevelBTN.TabIndex = 6;
+            this.maxLevelBTN.Text = "Get max level on all characters";
+            this.maxLevelBTN.UseVisualStyleBackColor = true;
+            this.maxLevelBTN.Click += new System.EventHandler(this.maxLevelBTN_Click);
+            // 
+            // tokensBTN
+            // 
+            this.tokensBTN.Location = new System.Drawing.Point(3, 227);
+            this.tokensBTN.Name = "tokensBTN";
+            this.tokensBTN.Size = new System.Drawing.Size(226, 57);
+            this.tokensBTN.TabIndex = 7;
+            this.tokensBTN.Text = "Get 999999999 tokens";
+            this.tokensBTN.UseVisualStyleBackColor = true;
+            this.tokensBTN.Click += new System.EventHandler(this.tokensBTN_Click);
+            // 
+            // zonesBTN
+            // 
+            this.zonesBTN.Location = new System.Drawing.Point(464, 227);
+            this.zonesBTN.Name = "zonesBTN";
+            this.zonesBTN.Size = new System.Drawing.Size(226, 57);
+            this.zonesBTN.TabIndex = 8;
+            this.zonesBTN.Text = "Unlock all zones (includes key zones)";
+            this.zonesBTN.UseVisualStyleBackColor = true;
+            this.zonesBTN.Click += new System.EventHandler(this.zonesBTN_Click);
+            // 
+            // keyBTN
+            // 
+            this.keyBTN.Location = new System.Drawing.Point(3, 290);
+            this.keyBTN.Name = "keyBTN";
+            this.keyBTN.Size = new System.Drawing.Size(226, 57);
+            this.keyBTN.TabIndex = 9;
+            this.keyBTN.Text = "Get key";
+            this.keyBTN.UseVisualStyleBackColor = true;
+            this.keyBTN.Click += new System.EventHandler(this.keyBTN_Click);
+            // 
+            // armorBTN
+            // 
+            this.armorBTN.Location = new System.Drawing.Point(235, 290);
+            this.armorBTN.Name = "armorBTN";
+            this.armorBTN.Size = new System.Drawing.Size(226, 57);
+            this.armorBTN.TabIndex = 10;
+            this.armorBTN.Text = "Get armor";
+            this.armorBTN.UseVisualStyleBackColor = true;
+            this.armorBTN.Click += new System.EventHandler(this.armorBTN_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(464, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 57);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Beat the game..?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // getcharBTN
+            // 
+            this.getcharBTN.Location = new System.Drawing.Point(239, 67);
+            this.getcharBTN.Name = "getcharBTN";
+            this.getcharBTN.Size = new System.Drawing.Size(226, 57);
+            this.getcharBTN.TabIndex = 14;
+            this.getcharBTN.Text = "Get character";
+            this.getcharBTN.UseVisualStyleBackColor = true;
+            this.getcharBTN.Click += new System.EventHandler(this.getcharBTN_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(266, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 27);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Get character by number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // charBox
+            // 
+            this.charBox.Location = new System.Drawing.Point(239, 39);
+            this.charBox.Name = "charBox";
+            this.charBox.Size = new System.Drawing.Size(226, 22);
+            this.charBox.TabIndex = 12;
+            // 
+            // dataBTN
+            // 
+            this.dataBTN.Location = new System.Drawing.Point(471, 67);
+            this.dataBTN.Name = "dataBTN";
+            this.dataBTN.Size = new System.Drawing.Size(226, 57);
+            this.dataBTN.TabIndex = 17;
+            this.dataBTN.Text = "Confirm";
+            this.dataBTN.UseVisualStyleBackColor = true;
+            this.dataBTN.Click += new System.EventHandler(this.dataBTN_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(519, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 27);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Inject data into slot";
+            // 
+            // dataBox
+            // 
+            this.dataBox.Location = new System.Drawing.Point(471, 39);
+            this.dataBox.Name = "dataBox";
+            this.dataBox.Size = new System.Drawing.Size(226, 22);
+            this.dataBox.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(239, 431);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(226, 43);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Open character list";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 486);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataBTN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataBox);
+            this.Controls.Add(this.getcharBTN);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.charBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.armorBTN);
+            this.Controls.Add(this.keyBTN);
+            this.Controls.Add(this.zonesBTN);
+            this.Controls.Add(this.tokensBTN);
+            this.Controls.Add(this.maxLevelBTN);
+            this.Controls.Add(this.getByte);
+            this.Controls.Add(this.getChipsBTN);
+            this.Controls.Add(this.saveSlotBTN);
+            this.Controls.Add(this.slotlabel);
+            this.Controls.Add(this.slotBox);
+            this.Controls.Add(this.getCharactersBTN);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FNaF World Mod Menu";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getCharactersBTN;
+        private System.Windows.Forms.TextBox slotBox;
+        private System.Windows.Forms.Label slotlabel;
+        private System.Windows.Forms.Button saveSlotBTN;
+        private System.Windows.Forms.Button getChipsBTN;
+        private System.Windows.Forms.Button getByte;
+        private System.Windows.Forms.Button maxLevelBTN;
+        private System.Windows.Forms.Button tokensBTN;
+        private System.Windows.Forms.Button zonesBTN;
+        private System.Windows.Forms.Button keyBTN;
+        private System.Windows.Forms.Button armorBTN;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button getcharBTN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox charBox;
+        private System.Windows.Forms.Button dataBTN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox dataBox;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
